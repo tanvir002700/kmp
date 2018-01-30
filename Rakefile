0815e7e16066969a3fa7910e1f485d9c0cc7fb17
tanvir hasan <tanvir002700@gmail.com>
2018-01-30 20:09:10 +0600

    setup c extension with ruby

--- Rakefile
+++ Rakefile
@@ -1,6 +1,5 @@
-require "bundler/gem_tasks"
-require "rspec/core/rake_task"
+require "rake/extensiontask"
 
-RSpec::Core::RakeTask.new(:spec)
-
-task :default => :spec
+Rake::ExtensionTask.new "kmp" do |ext|
+  ext.lib_dir = "lib/kmp"
+end
--- ext/kmp/kmp.c
+++ ext/kmp/kmp.c
@@ -5,8 +5,8 @@ static VALUE hello_world(VALUE klass)
   return rb_str_new2("hello world");
 }
 
-void Init_hello()
+void Init_kmp()
 {
-  VALUE mStree = rb_define_module("Hello");
+  VALUE mStree = rb_define_module("Kmp");
   rb_define_singleton_method(mStree, "hello_world", hello_world, 0);
 }
