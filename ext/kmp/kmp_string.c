686a86db10b611b6998c702dab3f887955809729
tanvir hasan <tanvir002700@gmail.com>
2018-01-31 23:44:46 +0600

    add controctor in ruby class through c

--- ext/kmp/kmp_string.c
+++ ext/kmp/kmp_string.c
@@ -1,11 +1,26 @@
 #include<ruby.h>
 
-static VALUE test(VALUE klass)
+static void deallocate(void * str)
 {
-  return rb_str_new2("hello world from test");
+    free((char *) str);
+}
+
+static void allocate(VALUE klass)
+{
+    char * str = malloc(sizeof(char));
+    return Data_Wrap_Struct(klass, NULL, deallocate, str);
+}
+
+static VALUE initialize(VALUE self)
+{
+    return self;
 }
 
 void Init_kmp_string(VALUE mKmp)
 {
     VALUE cKmpString = rb_define_class_under(mKmp, "String", rb_cObject);
+
+    rb_define_alloc_func(cKmpString, allocate);
+    rb_define_method(cKmpString, "initialize", initialize, 0);
+
 }
