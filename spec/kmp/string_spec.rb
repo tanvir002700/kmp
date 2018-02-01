0f863d5a121a98d1f29252c14e3fb22ebeefc4bf
tanvir hasan <tanvir002700@gmail.com>
2018-02-02 03:34:55 +0600

    setup rspec test config

---
+++ spec/kmp/string_spec.rb
@@ -0,0 +1,9 @@
+module Kmp
+  RSpec.describe String, type: :class do
+    context 'test' do
+      it 'should ok' do
+        expect(true).to eq(true)
+      end
+    end
+  end
+end
--- spec/kmp_spec.rb
+++ spec/kmp_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Kmp do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
 end
