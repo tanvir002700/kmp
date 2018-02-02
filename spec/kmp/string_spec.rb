c5e4339f9916f0546578bcded33024696fecc4fb
tanvir hasan <tanvir002700@gmail.com>
2018-02-02 23:07:10 +0600

    add test coverage for string

--- spec/kmp/string_spec.rb
+++ spec/kmp/string_spec.rb
@@ -1,8 +1,34 @@
 module Kmp
   RSpec.describe String, type: :class do
-    context 'test' do
-      it 'should ok' do
-        expect(true).to eq(true)
+    describe 'Kmp::String' do
+      context 'new method' do
+        it 'initialize object successfully' do
+          expect(String.new('abc')).to be_a String
+        end
+      end
+
+      context 'str accessor' do
+        it 'has str attribute accesor' do
+          expect(String.new('abcdek').str).to eq('abcdek')
+        end
+      end
+
+      context 'length method' do
+        it 'return initialize string length' do
+          expect(String.new('abdec').length).to eq(5)
+        end
+      end
+    end
+
+    describe '#match' do
+      it 'return all occurance of string' do
+        expect(String.new('ababacaababacaab').match('ababaca')).to match_array [0, 7]
+      end
+
+      it 'return 100 occurance' do
+        txt = ''
+        100.times { txt << 'abc'}
+        expect(String.new(txt).match('abc').size).to eq(100)
       end
     end
   end
