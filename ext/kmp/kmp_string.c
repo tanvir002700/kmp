ab2d25cd0863e5a193827da97c37aa44072e21d0
tanvir hasan <tanvir002700@gmail.com>
2018-02-04 01:36:38 +0600

    fix ISO C90 warnings

--- ext/kmp/kmp_string.c
+++ ext/kmp/kmp_string.c
@@ -27,7 +27,7 @@ static VALUE initialize(VALUE self, VALUE rb_string)
     Check_Type(rb_string, T_STRING);
     Data_Get_Struct(self, struct Str, str);
 
-    str->ptr = calloc(RSTRING_LEN(rb_string) , sizeof(char));
+    str->ptr = calloc(RSTRING_LEN(rb_string) + 1 , sizeof(char));
     memcpy(str->ptr, StringValuePtr(rb_string), RSTRING_LEN(rb_string));
 
     rb_iv_set(self, "@str", rb_string);
@@ -39,12 +39,11 @@ static VALUE initialize(VALUE self, VALUE rb_string)
 static int* compute_prefix(char *str)
 {
     int len = strlen(str);
-
-    int * prefix;
-    prefix = calloc(len+1, sizeof(int));
-
-    prefix[0] = -1;
     int k = -1;
+    int * prefix;
+
+    prefix = calloc(len+1, sizeof(int));
+    prefix[0] = -1;
 
     for(int i=1; i<len; i++)
     {
@@ -62,20 +61,21 @@ static VALUE match(VALUE self, VALUE rb_str)
     char * str;
     char * ptrn;
     int * prefix;
+    int n,m,q;
 
     Data_Get_Struct(self, struct Str, obj);
     str = calloc(strlen(obj->ptr), sizeof(char));
     strcpy(str, obj->ptr);
 
-    ptrn = calloc(RSTRING_LEN(rb_str), sizeof(char));
+    ptrn = calloc(RSTRING_LEN(rb_str) + 1, sizeof(char));
     memcpy(ptrn, StringValuePtr(rb_str), RSTRING_LEN(rb_str));
 
     prefix = compute_prefix(ptrn);
 
-    int n = strlen(str);
-    int m = strlen(ptrn);
+    n = strlen(str);
+    m = strlen(ptrn);
 
-    int q = -1;
+    q = -1;
     for(int i=0; i<n; i++)
     {
         while( q>-1 && ptrn[q+1]!=str[i] ) q = prefix[q];
