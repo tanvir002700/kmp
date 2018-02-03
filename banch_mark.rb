f4b0c84a9fc17eb9b7824be03165012b5ead497f
tanvir hasan <tanvir002700@gmail.com>
2018-02-04 01:30:16 +0600

    implement kmp in ruby for banch mark

--- banch_mark.rb
+++ banch_mark.rb
@@ -3,17 +3,57 @@
 require "bundler/setup"
 require "kmp"
 
+def compute_prefix(ptrn)
+  m = ptrn.size
+  prefix = Array.new m, 0
+  k = -1
+  prefix[0] = -1
+  (m-1).times do |i|
+    i = i + 1
+    while k>-1 && ptrn[k+1] != ptrn[i] do
+      k = prefix[k];
+    end
+    if ptrn[k+1] == ptrn[i]
+      k = k + 1
+    end
+    prefix[i] = k
+  end
+  prefix
+end
+
+def kmp_ruby(txt, ptrn)
+  prefix = compute_prefix(ptrn)
+  n = txt.size
+  m = ptrn.size
+  pos = []
+
+  q = -1
+  (n-1).times do |i|
+    i = i + 1
+    while(q>-1 && ptrn[q+1]!=txt[i]) do
+      q = prefix[q]
+    end
+    if ptrn[q+1] == txt[i]
+      q = q + 1
+    end
+    if q == (m-1)
+      pos << (i-m+1)
+      q = prefix[q]
+    end
+  end
+end
+
 #(0...50).map { ('a'..'z').to_a[rand(26)] }.join
 txt = ''
 10000.times{txt << 'abc'}
 
 t1 = Time.now
 a = Kmp::String.new txt
-b = a.match 'abcabcabcabcabcabc'
-puts Time.now - t1
-puts b.length
+a.match 'abcabcabcabcabcabc'
+puts (Time.now - t1).to_f
 
 t1 = Time.now
-b = txt.enum_for(:scan, /(?=abcabcabcabcabcabc)/).map { Regexp.last_match.offset(0).first }
+puts kmp_ruby(txt, 'abcabcabcabcabcabc')
 puts Time.now - t1
-puts b.size
+
+
