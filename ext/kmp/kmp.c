fa900d8d4659ba408e2abf09672b8b7221eb6314
tanvir hasan <tanvir002700@gmail.com>
2018-02-05 20:21:50 +0600

    remove unnecessary test method

--- ext/kmp/kmp.c
+++ ext/kmp/kmp.c
@@ -1,14 +1,8 @@
 #include<ruby.h>
 #include<kmp_string.h>
 
-static VALUE hello_world(VALUE klass)
-{
-  return rb_str_new2("hello world");
-}
-
 void Init_kmp()
 {
   VALUE mKmp = rb_define_module("Kmp");
-  rb_define_singleton_method(mKmp, "hello_world", hello_world, 0);
   Init_kmp_string(mKmp);
 }
